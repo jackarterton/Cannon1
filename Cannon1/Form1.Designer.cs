73f2fcc4e110eef882019092657e66fbbae5e740
Jack Arterton <jackarterton@gmail.com>
2012-12-20 00:04:48 +0000

    Cannon v1

---
+++ Cannon1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Cannon1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Projectile = new System.Windows.Forms.PictureBox();
+            this.Target = new System.Windows.Forms.PictureBox();
+            this.GameUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.LivesDisplay = new System.Windows.Forms.PictureBox();
+            this.TitleScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Projectile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LivesDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitleScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = false;
+            this.Player.Location = new System.Drawing.Point(303, 408);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(32, 32);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Projectile
+            // 
+            this.Projectile.Enabled = false;
+            this.Projectile.Location = new System.Drawing.Point(234, 374);
+            this.Projectile.Name = "Projectile";
+            this.Projectile.Size = new System.Drawing.Size(8, 8);
+            this.Projectile.TabIndex = 1;
+            this.Projectile.TabStop = false;
+            // 
+            // Target
+            // 
+            this.Target.Enabled = false;
+            this.Target.Location = new System.Drawing.Point(427, 51);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(32, 32);
+            this.Target.TabIndex = 2;
+            this.Target.TabStop = false;
+            // 
+            // GameUpdateTimer
+            // 
+            this.GameUpdateTimer.Enabled = true;
+            this.GameUpdateTimer.Interval = 16;
+            this.GameUpdateTimer.Tick += new System.EventHandler(this.GameUpdateTimer_Tick);
+            // 
+            // LivesDisplay
+            // 
+            this.LivesDisplay.Location = new System.Drawing.Point(4, 4);
+            this.LivesDisplay.Name = "LivesDisplay";
+            this.LivesDisplay.Size = new System.Drawing.Size(48, 16);
+            this.LivesDisplay.TabIndex = 3;
+            this.LivesDisplay.TabStop = false;
+            // 
+            // TitleScreen
+            // 
+            this.TitleScreen.Enabled = false;
+            this.TitleScreen.Location = new System.Drawing.Point(267, 201);
+            this.TitleScreen.Name = "TitleScreen";
+            this.TitleScreen.Size = new System.Drawing.Size(100, 50);
+            this.TitleScreen.TabIndex = 5;
+            this.TitleScreen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 452);
+            this.Controls.Add(this.TitleScreen);
+            this.Controls.Add(this.LivesDisplay);
+            this.Controls.Add(this.Target);
+            this.Controls.Add(this.Projectile);
+            this.Controls.Add(this.Player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cannon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Projectile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LivesDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitleScreen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Projectile;
+        private System.Windows.Forms.PictureBox Target;
+        private System.Windows.Forms.Timer GameUpdateTimer;
+        private System.Windows.Forms.PictureBox LivesDisplay;
+        private System.Windows.Forms.PictureBox TitleScreen;
+    }
+}
+
---
+++ Cannon1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cannon1
+{
+    public partial class Form1 : Form
+    {
+        const int SCREEN_WIDTH = 640;
+        const int SCREEN_HEIGHT = 480;
+
+        const int PROJECTILE_SPEED = 12;
+        const int BASE_TARGET_SPEED = 8;
+        const float COLLISION_DISTANCE = 20.0f;
+
+        const int STARTING_LIVES = 3;
+        const int GAME_DIFFICULTY_CONTROL = 3;
+        const int LIVES_TILE_WIDTH = 16;
+
+
+
+
+        int targetVelocityX;
+        int targetVelocityY;
+
+        bool isGameRunning = false;
+        int lives;
+        int level;
+
+
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(SCREEN_WIDTH, SCREEN_HEIGHT);
+            random = new Random();
+        }
+
+        #region events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Player.Load("player.png");
+            Target.Load("target.png");
+            Projectile.Load("projectile.png");
+            LivesDisplay.Load("lives.png");
+            TitleScreen.Load("title_screen.png");
+            TitleScreen.Left = 0;
+            TitleScreen.Top = 0;
+            TitleScreen.Width = SCREEN_WIDTH;
+            TitleScreen.Height = SCREEN_HEIGHT;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Player.Left = e.X - Player.Width / 2;
+            Player.Top = SCREEN_HEIGHT - Player.Height;
+        }
+
+        private void GameUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            Projectile.Top -= PROJECTILE_SPEED;
+
+            Target.Top += targetVelocityY;
+            Target.Left += targetVelocityX;
+
+            CheckGroundCollision();
+            CheckAirCollision();
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isGameRunning)
+            {
+                FireProjectile();
+            }
+            else
+            {
+                BeginGame();
+            }
+        }
+
+        #endregion
+
+        #region gameplay methods
+
+        private void BeginGame()
+        {
+            isGameRunning = true;
+            lives = STARTING_LIVES;
+            level = 1;
+
+            DisplayLives();
+            ResetProjectile();
+            ResetTarget();
+            
+            TitleScreen.Hide();
+            Cursor.Hide();
+        }
+
+        private void EndGame()
+        {
+            isGameRunning = false;
+            TitleScreen.Show();
+            Cursor.Show();
+        }
+
+        private void KillPlayer()
+        {
+            ResetTarget();
+
+            lives--;
+            DisplayLives();
+
+            if (lives == 0)
+            {
+                EndGame();
+            }
+        }
+
+        private void KillTarget()
+        {
+            level++;
+
+            ResetTarget();
+            ResetProjectile();
+        }
+
+        private void DisplayLives()
+        {
+            LivesDisplay.Width = lives * LIVES_TILE_WIDTH;
+        }
+
+        private void FireProjectile()
+        {
+
+            if (Projectile.Bottom < 0)
+            {
+                Projectile.Left = Player.Left + Player.Width / 2 - Projectile.Width / 2;
+                Projectile.Top = Player.Top - Projectile.Height;
+            }
+        }
+
+        private void ResetProjectile()
+        {
+            Projectile.Top = -Projectile.Height;
+        }
+
+        private void ResetTarget()
+        {
+            targetVelocityX = random.Next(2, 6);
+            targetVelocityY = BASE_TARGET_SPEED + level / GAME_DIFFICULTY_CONTROL;
+
+            if (random.Next(2) == 0)
+            {
+                targetVelocityX *= -1;
+            }
+
+            Target.Top = -Target.Height;
+            Target.Left = SCREEN_WIDTH / 2 - Target.Width / 2;
+        }
+
+        #endregion
+
+        #region collision methods
+
+        private void CheckGroundCollision()
+        {
+            if (Target.Bottom > SCREEN_HEIGHT)
+            {
+                KillPlayer();
+            }
+        }
+
+        private void CheckAirCollision()
+        {
+            Point projectileCenter = GetCenter(Projectile);
+            Point targetCenter = GetCenter(Target);
+
+            if (Distance(projectileCenter, targetCenter) < COLLISION_DISTANCE)
+            {
+                KillTarget();
+            }
+
+        }
+
+        #endregion
+
+        #region utilitymethods()
+
+        private Point GetCenter(Control c)
+        {
+            return new Point(c.Left + c.Width / 2, 
+                             c.Top + c.Height / 2);
+        }
+
+        private float Distance(Point pointA, Point pointB)
+        {
+            int a = pointA.X - pointB.X;
+            int b = pointA.Y - pointB.Y;
+
+            float c = (float)Math.Sqrt((a * a) + (b * b));
+
+            return c;
+        }
+
+        #endregion
+
+
+    }
+}
